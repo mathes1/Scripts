9ff65e1b94f8d568b746c79eb00933963d3040b6
Sean Matheson <mathes1@student.op.ac.nz>
2013-03-15 09:56:06 +1300

    Added my create user script and csv file

---
+++ NewADUsers.ps1
@@ -0,0 +1,30 @@
+#Script to create new active directory users
+#March 13 2013
+#Sean Matheson
+
+#import active direcory module
+Import-Module ActiveDirectory -ErrorAction SilentlyContinue
+
+#get domain DNS suffix
+$dnsroot = '@' + (Get-ADDomain).dnsroot
+
+#import the file containing new user account data and assign
+$users = Import-Csv .\NewADUsers.csv
+
+foreach ($user in $users)
+{
+	$newSamAccountName = $user.Username
+	if( -not (Get-ADUser -Filter {SamAccountName -eq $newSamAccountName})) 
+	{
+		New-ADUser -SamAccountName $user.Username -Name ($user.Firstname + " " + $user.Lastname) `
+		-DisplayName ($user.Firstname + " " + $user.Lastname) -GivenName $user.Firstname -Surname $user.Lastname `
+		-UserPrincipalName ($user.Username) `
+		-Enabled $true -ChangePasswordAtLogon $true -PasswordNeverExpires  $false `
+		-AccountPassword (ConvertTo-SecureString $user.Password -AsPlainText -force) -PassThru `
+		
+	}
+	else
+	{
+		echo "The Active directory account "$user.Username" is already registered to "$user.Firstname" "$user.Lastname""
+	}
+}
